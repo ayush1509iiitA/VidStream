28786ff5925b28e83b74b18bf0e851b108fb4d03
Ayush Chauhan <ayushnasa1509@gmail.com>
2024-09-04 06:54:52 +0530

    Initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,46 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+
+export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+
+export const categories = [
+  { name: "New", icon: <HomeIcon /> },
+  // { name: 'Ayush', icon: <CodeIcon />, },
+  { name: "Coding", icon: <CodeIcon /> },
+  { name: "ReactJS", icon: <CodeIcon /> },
+  { name: "NextJS", icon: <CodeIcon /> },
+  { name: "Music", icon: <MusicNoteIcon /> },
+  { name: "Education", icon: <SchoolIcon /> },
+  { name: "Podcast", icon: <GraphicEqIcon /> },
+  { name: "Movie", icon: <OndemandVideoIcon /> },
+  { name: "Gaming", icon: <SportsEsportsIcon /> },
+  { name: "Live", icon: <LiveTvIcon /> },
+  { name: "Sport", icon: <FitnessCenterIcon /> },
+  { name: "Fashion", icon: <CheckroomIcon /> },
+  { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Comedy", icon: <TheaterComedyIcon /> },
+  { name: "Gym", icon: <FitnessCenterIcon /> },
+  { name: "Crypto", icon: <DeveloperModeIcon /> },
+];
+
+export const demoThumbnailUrl =
+  "https://lh3.googleusercontent.com/pg52FEOeoYVpQXERZa3uJ6-UgdZUtJrdN4xi171PUru8qmmt1T77QaGPvT85OOX-XxQDGD7bm_ohJgg";
+export const demoChannelUrl = "channel/UCa4FC6a-6g7i0arN__K1W6A";
+export const demoVideoUrl =
+  "https://www.youtube.com/watch?v=hOHKltAiKXQ&pp=ygUJYmlnIGRhd2dz";
+export const demoChannelTitle = "Hanumankind";
+export const demoVideoTitle =
+  "Hanumankind – Big Dawgs | Ft. Kalmi (Official Music Video) | Def Jam India";
+export const demoProfilePicture =
+  "https://yt3.googleusercontent.com/_rO8IOjqWOx-nB8q3TX0Wag1McsdxxNgT2CF9m-oLU5U7H-p0QdEuPkDC37Hjldu7pjzd0X4Og=s160-c-k-c0x00ffffff-no-rj";
